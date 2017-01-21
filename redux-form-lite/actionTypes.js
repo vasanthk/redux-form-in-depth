47f979aff2587ec979a2436417138f2a22960295
vasanthk <vasa.httpster@gmail.com>
2017-01-20 23:27:44 -0800

    Form actions

---
+++ redux-form-lite/actionTypes.js
@@ -0,0 +1,4 @@
+export const INITIALIZE = 'redux-form-lite/INITIALIZE';
+export const DESTROY = 'redux-form-lite/DESTROY';
+export const SET_FIELD = 'redux-form-lite/SET_FIELD';
+export const TOUCH_ALL = 'redux-form-lite/TOUCH_ALL';
