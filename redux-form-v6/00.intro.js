d0dc0c55985fa517a3fe7bcb8505821361fa4e9c
vasanthk <vasa.httpster@gmail.com>
2017-01-31 00:06:00 -0800

    Redux form v6: Intro

---
+++ redux-form-v6/00.intro.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+class ContactForm extends Component {
+  render() {
+    const { handleSubmit } = this.props;
+    return (
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="firstName">First Name</label>
+          <Field name="firstName" component="input" type="text"/>
+        </div>
+        <div>
+          <label htmlFor="lastName">Last Name</label>
+          <Field name="lastName" component="input" type="text"/>
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <Field name="email" component="input" type="email"/>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
+}
+
+// Decorate the form component
+ContactForm = reduxForm({
+  form: 'contact' // a unique name for this form
+})(ContactForm);
+
+export default ContactForm;
+
+// Page
+class ContactPage extends React.Component {
+  handleSubmit = (values) => {
+    // Do something with the form values
+    console.log(values);
+  };
+
+  render() {
+    return (
+      <ContactForm onSubmit={this.handleSubmit}/>
+    );
+  }
+}
